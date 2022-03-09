72fb42a8dc27cd5e86c2e7e6606033afc0fa5ad4
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-09 21:06:03 +0100

    end project

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleall.css">
+    <title>Contact</title>
+</head>
+<body id="contact">
+        <section class="sidenav">
+        <a href="index.php"><span>AD</span>F⚽⚽T</a>   
+        <a href="inscription.php">Inscription</a>
+        <a href="connexion.php">Connexion</a>
+        </section>
+        <section class="contact">
+            <h1>Contact & Infos</h1>
+            Mon lien Github : <a href="https://github.com/fabien-allard/reservation-salles.git"><img src="../image/icons8-github-30.png" alt="Mon-Github" style="text-decoration:none;"/></a>
+            <p>Emails: fabien.allard@laplateforme.io</p>
+            <p>Adresse: 8 Rue d'Hozier</p>
+            <p>Marseille 13002</p>
+            <hr style="height: 0,5px; color: rgb(22, 172, 177); width: 50%; border: 1px straight #000;">
+            <p>Copyright © 2021-2022 </p>
+            <p>All rights reserved.</p>
+            <p>Created by DreamCod !</p>
+</div>
+        </section>
+    </article>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -33,22 +33,21 @@
         <h2>ADFOOT le foot en salle Marseillais</h2>
         <p>ADFOOT, c’est d’abord un Foot en Salle qui offre aux joueurs la possibilité de jouer sur des terrains synthétiques de dernière génération sans remplissage avec une sous couche de souplesse pour l’amélioration du confort de jeu.
         Nous fournissons les ballons et les chasubles. Veuillez noter que pour des raisons de sécurité, les joueurs portant des chaussures à crampons ne pourront pas accéder aux terrains.</p>
-        </section>
+        <a href="contact.php" id="contact">Infos et Contact</a>
+    </section>
 
         <section class="row2">
         <section class="column">
 
     <a href="inscription.php">S'inscrire</a>
     </section>
+
         <section class="column">
     <a href="connexion.php">Se connecter</a>
     </section>
     </section>
     </section>
     </section>
-
     </main>
-
-
 </body>
 </html>
