df07c99937a0c007392c1311f76ae05754adbf8f
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2022-03-09 18:21:26 +0100

    retravailler

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+<link rel="stylesheet" href="styleall.css">
+<link href="https://fonts.googleapis.com/css2?family=Scada&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+<header>
+    <section class="index">
+
+    <?php
+  // Si l'utilisateur est loggé le header est personnalisé
+    if (isset($_SESSION['login']))
+    {
+        echo '<a href="profil.php">   Tu es connecté(e)      ' . $_SESSION['login'] . '</a>' . '<a href="planning.php"> accéder au planning </a>' . '<a href="profil.php?deconnexion"> deconnexion </a>';
+    }
+    else
+    { ?>
+        <?php
+    } ?>
+
+    </header>
+
+
+    <main>
+        <section class="main">
+
+        <section class="header">
+        <h2>ADFOOT le foot en salle Marseillais</h2>
+        <p>ADFOOT, c’est d’abord un Foot en Salle qui offre aux joueurs la possibilité de jouer sur des terrains synthétiques de dernière génération sans remplissage avec une sous couche de souplesse pour l’amélioration du confort de jeu.
+        Nous fournissons les ballons et les chasubles. Veuillez noter que pour des raisons de sécurité, les joueurs portant des chaussures à crampons ne pourront pas accéder aux terrains.</p>
+        </section>
+
+        <section class="row2">
+        <section class="column">
+
+    <a href="inscription.php">S'inscrire</a>
+    </section>
+        <section class="column">
+    <a href="connexion.php">Se connecter</a>
+    </section>
+    </section>
+    </section>
+    </section>
+
+    </main>
+
+
+</body>
+</html>
---
+++ reservationsalles.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 09 mars 2022 à 16:42
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `reservationsalles`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `debut` datetime NOT NULL,
+  `fin` datetime NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=145 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_utilisateur`) VALUES
+(144, 'teamwork', 'Ã©quipe de 4 joueurs contre 4', '2022-03-10 14:00:00', '2022-03-10 15:00:00', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
+(4, 'choco5', '$2y$10$Ijy8lQHotOG66nQxZ3oi.e4fl3u5eexyAUEV7vS6INvdXskLi0WIq'),
+(5, 'teste1', '$2y$10$MTeGHYZIk1i4bcbCfwaXa.P2YWQp8jTROAVu00qj20b2STV9YbiXy');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
